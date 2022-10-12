f85bf69d8ed931d4c80237ce5ddcd9bb8102792b
Vegascruz <cruzv@students.westerntc.edu>
2022-10-11 20:07:06 -0500

    Was able to find/create a url that grabbed 5 days worth of weather data based on the city. Just need to add user input for city... then add the data to the page

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,12 @@
 var city = "Las Vegas";
 var API_Key = "b9484f46952004ac40fe78867828c4ee";
-var lat = 0;
-var lon = 0;
+var url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=5&units=imperial&appid=${API_Key}`;
 
-fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_Key}`)
+fetch(url)
 .then(response => response.json())
 .then(data => {
     console.log(data)
-    lat = data[0]['lat'];
-    lon = data[0]['lon'];
-    console.log("longitude: " + lon + "latitude: " + lat);
+
 });
 
 
