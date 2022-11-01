24b0eaf79db7a46cdfd4668251dcc412593220d7
Vegascruz <cruzv@students.westerntc.edu>
2022-10-31 19:35:22 -0500

    Did some of the styling and added code to grab dates for today and forecast

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,30 @@
 var city = "Las Vegas";
 var API_Key = "b9484f46952004ac40fe78867828c4ee";
-var url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=5&units=imperial&appid=${API_Key}`;
+var count = 5;
+var url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&exclude=minutely,hourly,alerts&appid=${API_Key}`;
 
-fetch(url)
-.then(response => response.json())
-.then(data => {
-    console.log(data)
+var url_two = `https://api.openweathermap.org/data/2.5/onecall?lat=36.114647&lon=-115.172813&appid=${API_Key}`;
 
-});
+let todaysDay = moment().format("M/D/YYYY");
+var h1 = $("#current").children("h1").text();
+console.log(h1);
+
+for(i=0;i<5;i++){
+
+    console.log(i);
+}
+
+function fetchData(){
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+    /*for(i=0; i<5; i++){
+        console.log(data.list[i].main.temp);
+    }*/
+    });
+}
+
+fetchData();
+
 
 
